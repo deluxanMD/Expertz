a0a343c10b55bd32d15576e80bbbda4bfd4a0176
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-02-20 20:55:49 +0530

    not needed

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,4 @@
 /* eslint-disable no-undef */
-import 'react-native-gesture-handler/jestSetup';
-
 jest.mock('@react-navigation/native', () => ({
   ...jest.requireActual('@react-navigation/native'),
   useNavigation: jest.fn(),
